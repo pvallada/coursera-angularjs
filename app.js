4c6c8445bb1035fe3e6c3be5eb53577a33d24a66
Pedro Valladares <pedro.valladares@gmail.com>
2017-02-13 02:42:38 -0500

    module 2 assignment

--- app.js
+++ app.js
@@ -1,28 +1,84 @@
 (function () {
     'use strict';
-  angular.module('mode1App', [])
-      .controller('mod1Controller', function ($scope) {
-          $scope.lunch = "";
-          $scope.msg = "";
-          $scope.displayMsg = function(){
-              var itemCount = 0;
-              var arrayOfStrings = $scope.lunch.split(',');
-              var arrayLength = arrayOfStrings.length;
-              for (var i = 0; i < arrayLength; i++) {
-                  //ignore empty items
-                  if ( !(arrayOfStrings[i].length === 0 || !arrayOfStrings[i].trim())){
-                      itemCount++;
-                  }
-              }
-              if ( itemCount > 0 && itemCount <= 3){
-                  $scope.msg = "Enjoy!";
-              }
-              else if (itemCount > 3 ){
-                  $scope.msg = "Too much!";
-              }
-              else {
-                  $scope.msg = "Please enter data first";
-              }
-          };
-      });
+  angular.module('ShoppingListCheckOff', [])
+      .controller('ToBuyController', ToBuyController)
+      .controller('AlreadyBoughtController', AlreadyBoughtController)
+      .service('ShoppingListCheckOffService', ShoppingListCheckOffService);
+
+    ToBuyController.$inject = ['ShoppingListCheckOffService'];
+    function ToBuyController(ShoppingListCheckOffService) {
+        var toBuyCtrl = this;
+
+        toBuyCtrl.itemName = "";
+        toBuyCtrl.itemQuantity = "";
+
+        toBuyCtrl.addItem = function () {
+            ShoppingListCheckOffService.addItemtoBuy(toBuyCtrl.itemName, toBuyCtrl.itemQuantity);
+        }
+        toBuyCtrl.removeItem = function (itemIndex) {
+            ShoppingListCheckOffService.removeToBuyItem(itemIndex);
+        };
+        toBuyCtrl.items = ShoppingListCheckOffService.getToBuyItems();
+    }
+
+
+    AlreadyBoughtController.$inject = ['ShoppingListCheckOffService'];
+    function AlreadyBoughtController(ShoppingListCheckOffService) {
+        var bougthListCtrl = this;
+
+        bougthListCtrl.items = ShoppingListCheckOffService.getBoughtItems();
+        bougthListCtrl.removeItem = function (itemIndex) {
+            ShoppingListCheckOffService.removeBoughtItem(itemIndex);
+        };
+    }
+
+
+    function ShoppingListCheckOffService() {
+        var service = this;
+
+        // List of items to buy
+        var toBuy = [ {name:'Cookies', quantity:5}, {name: 'bottle of juice', quantity:3},{name: 'Candy', quantity:8}, {name: 'Package of milk', quantity:3},{name: 'Flour', quantity:7}];
+        // List of bought items
+        var bought = [];
+
+        // function to add items to buy
+        service.addItemtoBuy = function (itemName, quantity) {
+            var item = {
+                name: itemName,
+                quantity: quantity
+            };
+            toBuy.push(item);
+        };
+
+        service.removeToBuyItem = function (itemIndex) {
+            // get the item before deleting it
+            var item = toBuy[itemIndex];
+            // remove the item from to buy list
+            toBuy.splice(itemIndex, 1);
+            // call the service to add item to the bought list
+            service.addItemtoBoughtList(item.name,item.quantity);
+        };
+
+        // function to return the list of items to buy
+        service.getToBuyItems = function () {
+            return toBuy;
+        };
+
+        // function to add items that were bought
+        service.addItemtoBoughtList = function (itemName, quantity) {
+            var item = {
+                name: itemName,
+                quantity: quantity
+            };
+            bought.push(item);
+        };
+        service.removeBoughtItem = function (itemIdex) {
+            bought.splice(itemIdex, 1);
+        };
+        // function to return the list of bought items
+        service.getBoughtItems = function () {
+            return bought;
+        };
+    }
+
 })();
